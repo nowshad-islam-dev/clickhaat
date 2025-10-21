cffb25eaeb268fb2f76412e9c5ff2180e10aa986
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-21 18:45:49 +0600

    add image configs according to folder name

---
+++ server/src/media-config/imageRules.js
@@ -0,0 +1,22 @@
+exports.imageRules = {
+  categories: {
+    width: 400,
+    height: 400,
+    quality: 80,
+  },
+  products: {
+    width: 640,
+    height: 640,
+    quality: 85,
+  },
+  banners: {
+    width: 1920,
+    height: 600,
+    quality: 75,
+  },
+  default: {
+    width: 800,
+    height: 800,
+    quality: 80,
+  },
+};
