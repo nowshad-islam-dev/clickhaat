de24ab64eaba0e716009d47efbdd350bf3893e6b
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-28 23:37:49 +0600

    refactor error messges

--- server/src/validators/product.validators.js
+++ server/src/validators/product.validators.js
@@ -12,7 +12,7 @@ exports.validateProductCreationRequest = [
   body('price')
     .optional()
     .isFloat({ min: 0 })
-    .withMessage('Price must be a non-negative number'),
+    .withMessage('Price must be a non-negative number.'),
 
   body('description')
     .trim()
@@ -36,46 +36,46 @@ exports.validateProductCreationRequest = [
     .isFloat({ min: 0, max: 100 })
     .withMessage('Offer must be between 0 and 100.'),
 
-  body('picture').optional().isArray().withMessage('Picture must be an array'),
+  body('picture').optional().isArray().withMessage('Picture must be an array.'),
 
   body('picture.*url')
     .optional({ nullable: true })
     .isString()
-    .withMessage('Picture URL must be a string')
+    .withMessage('Picture URL must be a string.')
     .isLength({ max: 2048 })
-    .withMessage('Picture URL is too long'),
+    .withMessage('Picture URL is too long.'),
 
   body('picture.*.alt')
     .optional({ nullable: true })
     .trim()
     .escape()
     .isLength({ max: 20 })
-    .withMessage('Alt text must be at most 20 characters'),
+    .withMessage('Alt text must be at most 20 characters.'),
 
   body('picture.*.isPrimary')
     .optional()
     .isBoolean()
-    .withMessage('isPrimary must be a boolean'),
+    .withMessage('isPrimary must be a boolean.'),
 
-  body('review').optional().isArray().withMessage('Review must be an array'),
+  body('review').optional().isArray().withMessage('Review must be an array.'),
 
   body('review.*.user')
     .optional()
     .isMongoId()
-    .withMessage('Review user must be a valid MongoDB ObjectId'),
+    .withMessage('Review user must be a valid MongoDB ObjectId.'),
 
   body('review.*.text')
     .optional()
     .trim()
     .escape()
     .isLength({ max: 1000 })
-    .withMessage('Review text must be at most 1000 characters'),
+    .withMessage('Review text must be at most 1000 characters.'),
 
   body('category')
     .notEmpty()
     .withMessage('Category must be provided.')
     .isMongoId()
-    .withMessage('Category must be a valid ObjectId'),
+    .withMessage('Category must be a valid ObjectId.'),
 ];
 
 exports.validateProductUpdateRequest = [
@@ -83,7 +83,7 @@ exports.validateProductUpdateRequest = [
     .notEmpty()
     .withMessage('Product Id is missing in params.')
     .isMongoId()
-    .withMessage('Product Id must be a valid ObjectId'),
+    .withMessage('Product Id must be a valid ObjectId.'),
 
   body('name')
     .optional()
@@ -97,7 +97,7 @@ exports.validateProductUpdateRequest = [
   body('price')
     .optional()
     .isFloat({ min: 0 })
-    .withMessage('Price must be a non-negative number'),
+    .withMessage('Price must be a non-negative number.'),
 
   body('description')
     .optional()
@@ -133,5 +133,5 @@ exports.validateProductDeleteRequest = [
     .notEmpty()
     .withMessage('Product Id is missing in params.')
     .isMongoId()
-    .withMessage('Product Id must be a valid ObjectId'),
+    .withMessage('Product Id must be a valid ObjectId.'),
 ];
