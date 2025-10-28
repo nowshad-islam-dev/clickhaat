82b40e49dc54e92c2e5017d89183fd1087cc9d24
nowshad-islam-dev <mdnowshadislam.2108033.ruet.mte@gmail.com>
2025-10-28 23:37:12 +0600

    add validators for category update and delete

--- server/src/validators/category.validators.js
+++ server/src/validators/category.validators.js
@@ -1,4 +1,4 @@
-const { body } = require('express-validator');
+const { body, param } = require('express-validator');
 
 exports.validateCategoryCreationRequest = [
   body('name')
@@ -12,27 +12,55 @@ exports.validateCategoryCreationRequest = [
   body('parentId')
     .optional()
     .isMongoId()
-    .withMessage('Category must be a valid ObjectId'),
+    .withMessage('Parent Category must be a valid ObjectId.'),
 
   body('image').optional().isObject().withMessage('Image must be an object.'),
 
   body('image.*url')
     .optional({ nullable: true })
     .isString()
-    .withMessage('Image URL must be a string')
+    .withMessage('Image URL must be a string.')
     .isLength({ max: 2048 })
-    .withMessage('Image URL is too long'),
+    .withMessage('Image URL is too long.'),
 
   body('image.*.alt')
     .optional({ nullable: true })
     .trim()
     .escape()
     .isLength({ max: 20 })
-    .withMessage('Alt text must be at most 20 characters'),
+    .withMessage('Alt text must be at most 20 characters.'),
 
   body('image.*.isPrimary')
     .optional()
     .isBoolean()
-    .withMessage('isPrimary must be a boolean'),
+    .withMessage('isPrimary must be a boolean.'),
   ,
+];
+
+exports.validateCategoryUpdateRequest = [
+  param('categoryId')
+    .notEmpty()
+    .withMessage('Category Id is missing in params.')
+    .isMongoId()
+    .withMessage('Category Id must be a valid ObjectId.'),
+
+  body('name')
+    .optional()
+    .trim()
+    .escape()
+    .isLength({ min: 3, max: 30 })
+    .withMessage('Category name must be between 3 and 30 characters.'),
+
+  body('parentId')
+    .optional()
+    .isMongoId()
+    .withMessage('Parent Category must be a valid ObjectId.'),
+];
+
+exports.validateCategoryDeleteRequest = [
+  param('categoryId')
+    .notEmpty()
+    .withMessage('Category Id is missing in params.')
+    .isMongoId()
+    .withMessage('Category Id must be a valid ObjectId.'),
 ];
